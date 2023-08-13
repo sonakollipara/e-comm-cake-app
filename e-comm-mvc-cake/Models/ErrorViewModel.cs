95d66f817efa295fc3a3f922943f82d6ff864a95
kollipara <sonakollipara@gmail.com>
2023-08-13 10:37:27 +0530

    created project folder in vs code

---
+++ e-comm-mvc-cake/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e_comm_mvc_cake.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
